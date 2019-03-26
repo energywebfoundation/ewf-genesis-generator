717d944fdd398376a55c1e13d2f730b18fc70da0
danzipie <danzipie@gmail.com>
2019-03-26 17:46:56 +0100

    add multisig

--- README.md
+++ README.md
@@ -13,14 +13,10 @@ npm install
 
 - **Usage**:
    To generate chain spec and include all system contracts (according to DEFAULT_CONTRACT_CONFIGS within index.js file):
+- **Requirements**:
+   - Compiled system contracts and Gnosis MultiSigWallet contract
 
-node . --p [COMPILED-CONTRACTS-PATH] 
+node . --p [COMPILED-CONTRACTS-PATH] -m [COMPILED-MULTISIG-PATH]
 ```
-node . --p ../genome-system-contracts/build/contracts
-```
-   - To generate for specific contract:
-
-node . --p [COMPILED-CONTRACT-PATH] --cn [DESIRED-CONTRACT-NAME] --ca [DESIRED-CONTRACT-ADDRESS]
-```
-node . --p ../genome-system-contracts/build/contracts --cn ValidatorFoo --ca 0x1000000000000000000000000000000000000005
+node . -p ../genome-system-contracts/build/contracts/ -m ../MultiSigWallet/build/contracts/
 ```
--- index.js
+++ index.js
@@ -11,14 +11,9 @@ const ganache = require("ganache-core");
 // parse argument variables
 const args = require('minimist')(process.argv.slice(2));
 
-// default settings
-const USING_LOCAL_CHAINSPEC = true;
-
 // argument variable settings
 const CONTRACTS_PATH = args['p'];
-const SINGLE_CONTRACT = (args['cn'] != undefined);
-const SINGLE_CONTRACT_NAME = args['cn'];
-const SINGLE_CONTRACT_ADDRESS = args['ca'];
+const MULTISIG_PATH = args['m'];
 const BUILD_PATH = args['b'];
 
 // default contract config
@@ -28,6 +23,21 @@ const REWARD = '0x1204700000000000000000000000000000000002';
 const COMMUNITY_FUND = '0x1204700000000000000000000000000000000003';
 
 var DEFAULT_CONTRACT_CONFIGS = [
+    {
+        address: VALIDATOR_RELAYED,
+        name: 'ValidatorSetRelayed',
+        description: 'Validator Set Relayed',
+        params: [
+            VALIDATOR_RELAY,
+            [
+                // this is the list of initial validators
+                "0x7e8b8661dbc77d6bee7a1892fbcf8ef6378cab30",
+                "0xdae561c716f9ea58e32e37d9ae95465eca286012",
+                "0xebee2fc556975c3dd50c17d13a15af535fb7bbb3"
+            ]
+        ],
+        params_types: ['address', 'address[]']
+    },
     {        
         address: VALIDATOR_RELAY,
         name: 'ValidatorSetRelay',
@@ -36,20 +46,6 @@ var DEFAULT_CONTRACT_CONFIGS = [
             VALIDATOR_RELAYED
         ],
         params_types: ['address']
-    },
-    {
-        address: VALIDATOR_RELAYED,
-        name: 'ValidatorSetRelayed',
-        description: 'Validator Set Relayed',
-        params: [
-            VALIDATOR_RELAY,
-            [
-                "0x7e8b8661dbc77d6bee7a1892fbcf8ef6378cab30",
-                "0xdae561c716f9ea58e32e37d9ae95465eca286012",
-                "0xebee2fc556975c3dd50c17d13a15af535fb7bbb3"
-            ]
-        ],
-        params_types: ['address', 'address[]']
     },
     {
         address: REWARD,
@@ -63,6 +59,19 @@ var DEFAULT_CONTRACT_CONFIGS = [
     }
 
 ];
+
+var DEFAULT_MULTISIG_CONFIGS = [
+    {        
+        address: COMMUNITY_FUND,
+        name: 'MultiSigWallet',
+        description: 'Multi Signature Wallet',
+        params: [
+            // list of netops addresses
+            ['0x0650231bd8ebb81af7aeeee52e322eeb28fea5b9']
+        ],
+        params_types: ['address[]']
+    }  
+]
 
 var web3 = new Web3(ganache.provider());
 
@@ -103,39 +112,51 @@ function retrieveChainspec(callback) {
 function retrieveContractsBytecode(callback) {   
     console.log('## retrieving specifid contracts bytecodes ##');
 
-    if (SINGLE_CONTRACT) {
-        // retrieving the compiled contract bytecode
-        fs.readFile(CONTRACTS_PATH + '/' + SINGLE_CONTRACT_NAME + '.json', (err, contractJson) => {  
-            if (err) throw err;            
-            chainspec.accounts[SINGLE_CONTRACT_ADDRESS] = {
-                balance: '1',
-                constructor: JSON.parse(contractJson).bytecode
-            };            
-            callback(null);
-        });
-    } else {
-        if (CONTRACTS_PATH != undefined){
-            async.each(DEFAULT_CONTRACT_CONFIGS, function(contractConfig, callback) {        
-                // retrieving the compiled contract bytecode
-                fs.readFile(CONTRACTS_PATH + '/' + contractConfig.name + '.json', (err, contractJson) => {  
-                    if (err) throw err;
-
-                    let _constructor = encodeParamToByteCode(JSON.parse(contractJson).bytecode, contractConfig.params_types, contractConfig.params);
-                    chainspec.accounts[contractConfig.address] = {
-                        balance: '1',
-                        constructor: _constructor
-                    };
-                    callback(null);
-                });        
-              }, function(err) {
-                if (!err){
-                    callback(null);
-                }
-            });
-        } else {
-            console.log("Compiled built contract path no defined, usage: --p <YOUR_COMPILED_CONTRACT_PATH>");
-        }  
-    }
+    async.series([
+        function(callback) {
+            if (MULTISIG_PATH != undefined){
+                async.each(DEFAULT_MULTISIG_CONFIGS, function(contractConfig, callback) { 
+                    fs.readFile(MULTISIG_PATH + '/' + contractConfig.name + '.json', (err, contractJson) => {
+                        if (err) throw err;
+            
+                        let _constructor = encodeParamToByteCode(JSON.parse(contractJson).bytecode, contractConfig.params_types, contractConfig.params);
+                        chainspec.accounts[contractConfig.address] = {
+                            balance: '1',
+                            constructor: _constructor
+                        };
+                        callback(null);
+                    });
+                }, function(err) {
+                    if (!err){
+                        callback(null);
+                    }
+                });
+            }
+        },
+        function(callback) {
+            if (CONTRACTS_PATH != undefined){
+                async.each(DEFAULT_CONTRACT_CONFIGS, function(contractConfig, callback) {        
+                    // retrieving the compiled contract bytecode
+                    fs.readFile(CONTRACTS_PATH + '/' + contractConfig.name + '.json', (err, contractJson) => {
+                        if (err) throw err;
+        
+                        let _constructor = encodeParamToByteCode(JSON.parse(contractJson).bytecode, contractConfig.params_types, contractConfig.params);
+                        chainspec.accounts[contractConfig.address] = {
+                            balance: '1',
+                            constructor: _constructor
+                        };
+                        callback(null);
+                    });        
+                    }, function(err) {
+                    if (!err){
+                        callback(null);
+                    }
+                });
+            } else {
+                console.log("Compiled built contract path no defined, usage: --p <YOUR_COMPILED_CONTRACT_PATH>");
+            }
+        }
+    ])
 }
 
 function addPoaParams(callback) {
@@ -160,4 +181,4 @@ function encodeParamToByteCode(bytecode, parameterTypes, parameterValues) {
     parameters = web3.eth.abi.encodeParameters(parameterTypes, parameterValues);
     //merge bytecode and parameters
     return bytecode.concat(parameters.slice(2))
-  }
+}
