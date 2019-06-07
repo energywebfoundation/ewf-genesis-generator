7f2384999b84ea531b6b6f27a915ea018c12d5a6
ngyam <ngy.am.zs@gmail.com>
2019-06-07 19:21:20 +0200

    fix(ewc-chainspec): fixes
    - fixed amounts according to final token doc
    - updated packages
    - build script uses latest ewc contracts tag by default

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 cdir="$( cd "$(dirname "$0")" ; pwd -P )"
 
 CONTRACT_REPO_NAME=${CONTRACT_REPO_NAME:-"ewc-system-contracts"}
-TAG=${TAG:-"v1.0.1"}
+TAG=${TAG:-"v1.1.0"}
 
 if [[ ${1} == "install" ]]; then
 	npm install
